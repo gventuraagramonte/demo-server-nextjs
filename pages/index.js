b52de783971b4ed902415250ff4865b442632b13
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-10-07 21:30:38 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Head from "next/head";
+import { NewRelicSnippet } from "../components/NewRelicSnippets";
 
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
-  )
+    <>
+      <Head>
+        <title>Custom Server</title>
+      </Head>
+      <ul>
+        <li>
+          <Link href="/b" as="/a">
+            <a>a</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/a" as="/b">
+            <a>b</a>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const { createServer } = require('http')
-const next = require('next')
+const { createServer } = require("http");
+const next = require("next");
+const newrelic = require("newrelic");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = new URL(req.url, 'http://w.w')
-    const { pathname, query } = parsedUrl
+    const parsedUrl = new URL(req.url, "http://w.w");
+    const { pathname, query } = parsedUrl;
+    newrelic.setTransactionName(pathname); // configurate with your path
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
+    if (pathname === "/a") {
+      app.render(req, res, "/a", query);
+    } else if (pathname === "/b") {
+      app.render(req, res, "/b", query);
     } else {
-      handle(req, res, parsedUrl)
+      handle(req, res, parsedUrl);
     }
   }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
